b04c594940d4c40b6d9a16d764e978ff7206cbf1
jimmykdunn <jkdunn@bu.edu>
2019-09-12 12:21:35 -0400

    Found first image with crawler!

--- rosbag_crawler.py
+++ rosbag_crawler.py
@@ -20,6 +20,7 @@ INFO:
 
 import numpy as np
 import struct
+import matplotlib.pyplot as plt
 
 def rosbag_crawler(bagfile):
     # Open up the bag
@@ -44,7 +45,7 @@ def rosbag_crawler(bagfile):
         # This allows easy random access
         recordCount = 0
         #while True:
-        for i in range(3):
+        for i in range(10):
             header_len = struct.unpack('i', bagptr.read(4))[0]
             header = bagptr.read(header_len)
             isChunk = parse_header(header)
@@ -55,15 +56,51 @@ def rosbag_crawler(bagfile):
             data = bagptr.read(data_len)
             
             if isChunk:
-                # Interesting stuff here. Figure out how to parse it
-                bf = 1
+                ick = 0
+                while ick < len(str(data)):
+                    #header_len = struct.unpack('i', data[ick:ick+4])[0]
+                    #ick += 4
+                    #header = data[ick:ick+header_len]
+                    #ick += header_len
+                    #parse_header(header,'    ')
+                    #if 'topic=/quad_bb9e/face_detection/image' in str(data)[ick:]:
+                    #if 'type=sensor_msgs/Image' in str(data)[ick:]:
+                    #if 'bgr' in str(data)[ick:]:
+                    if data[ick:].find(b'bgr') != -1:
+                        imageIdx = data[ick:].find(b'bgr')
+                        print(str(data[ick+imageIdx-2000:ick+imageIdx-500]))
+                        starti = ick+imageIdx
+                        
+                        ny = struct.unpack('i', data[starti-12:starti-8])[0]
+                        nx = struct.unpack('i', data[starti-8:starti-4])[0]
+                        bpp=1
+                        encoding = struct.unpack('i', data[starti-4:starti])[0]
+                        bgr = str(data[starti:starti+3])
+                        img0 = starti+3 + 10
+                        imgNPix = nx*ny*3
+                        imgNBytes = nx*ny*3*bpp
+                        imgBytes = data[img0:img0+imgNBytes]
+                        imageFlat = np.zeros([imgNPix,1]).astype('uint32')
+                        for ip in range(imgNPix):
+                            #imageFlat[ip] = struct.unpack('uint8', imgBytes[bpp*ip:bpp*ip+bpp])[0]
+                            imageFlat[ip]=ord(imgBytes[bpp*ip:bpp+bpp*ip])
+                        image = np.reshape(imageFlat,[ny,nx,3])  
+                        #imageStrIdx = str(data)[ick:].find(b'bgr')
+                        #imageStrIdx = str(data)[ick:].find(b'bgr')
+                        #print(str(data)[ick+imageStrIdx:ick+imageStrIdx+2000])
+                        #print(str(data)[ick+imageStrIdx:ick+imageStrIdx+1000])
+                        plt.imshow(image)
+                        ick += imageIdx+1
+                        return
+                   
+                    
+                    bf = 1
                 
-                #/quad_bb93/face_detection/image appears to prepend imagery
+                #/quad_bb9e/face_detection/image appears to prepend imagery
                 # and it occurs in the middle of data chunks. Search for it!
                 
                 # Additionally, there is a bunch of text describing it under
                 # "message_definition"
-                pass
                 
             recordCount += 1
             print('-------------------')
@@ -81,10 +118,10 @@ def rosbag_crawler(bagfile):
 # end rosbag_crawler()
     
 # Parses and prints a header    
-def parse_header(header):
+def parse_header(header,spaces=''):
     isChunk = False
     i = 0
-    print('HEADER')
+    print(spaces+'HEADER')
     while i<len(header):
         # Read field length
         fieldLen = struct.unpack('i', header[i:i+4])[0]
@@ -94,14 +131,17 @@ def parse_header(header):
         iFieldStart = i
         ifield = 0
         fieldName = ''
-        while chr(header[i]) != '=':
+        while chr(header[i]) != '=' and ifield < fieldLen:
             fieldName += chr(header[i])
             i += 1
             ifield += 1
+            if i >= len(header):
+                break
         i += 1    
         
         # Read field value
         fieldSize = iFieldStart + fieldLen - i
+        printField = True
         
         if fieldName == 'op':
             if header[i:i+fieldSize] == b'\x02':
@@ -131,13 +171,20 @@ def parse_header(header):
             value = struct.unpack(fieldType, header[i:i+fieldSize])[0]
         else:
             value = header[i:i+fieldSize]
+            printField = False
             
-        print('    '+fieldName + '=' + str(value))
-        i += fieldSize
+        if printField:
+            print(spaces+'    '+fieldName + '=' + str(value))
+        else:
+            print(spaces+'    Field size: '+ str(fieldSize))
+        if fieldSize < 0:
+            stophere = 0
+        else:
+            i += fieldSize
         
     return isChunk
 
 # Run with defaults if at highest level
 if __name__ == "__main__":
-    rosbag_crawler("C:\\Users\\jimmy\\OneDrive\\Documents\\gradSchool\\thesis\\data\\rosbags\\greenTest_01.bag")
+    rosbag_crawler("C:\\Users\\jimmy\\OneDrive\\Documents\\gradSchool\\thesis\\data\\rosbags\\helloworld.bag")
     
