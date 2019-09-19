5972dab33a509ab5ed0eb85148566d2aacb3a608
James Kenneth Dunn <jkdunn@geo.bu.edu>
2019-09-18 21:36:41 -0400

    Adding loadModules.py for scc1.bu.edu usage

---
+++ loadModules.py
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Loads the requisite modules - intended for use on Boston University shared computing cluster (scc1.bu.edu)
+module load python3/3.6.5
+echo "Loaded python3/3.6.5"
+module load opencv/4.0.1
+echo "Loaded opencv/4.0.1"
+module load tensorflow/1.13.1
+echo "Loaded tensorflow/1.13.1"
