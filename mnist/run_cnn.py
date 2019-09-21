433146ee165a57ba13e4b7631994e2eea2aa86e3
James Kenneth Dunn <jkdunn@geo.bu.edu>
2019-09-21 14:22:51 -0400

    Cleaning up and adding checkpoint saving

--- mnist/run_cnn.py
+++ mnist/run_cnn.py
@@ -29,9 +29,10 @@ import matplotlib.pyplot as plt
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
-
-def makeBatch(length, x, y, epoch):
-    """Pulls a batch of the input length from the input data"""
+# Extract a batch of data of the input length from data x and truth y. Epoch
+# is input to ensure that we extract every image over the course of many
+# consecutive epochs.
+def extractBatch(length, x, y, epoch):
     
     # Just pull them in order, wrapping to the beginning when we go over
     while (epoch+1)*length > len(x):
@@ -47,21 +48,18 @@ def makeBatch(length, x, y, epoch):
     
     return x_batch, y_batch
 
+# Constructs a weight variable with the given shape (random initialization)
 def weight_variable(shape):
-    """weight_variable generates a weight variable of a given shape."""
     initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial)
 
-
+# Constructs a bias variable with the given shape (constant initialization)
 def bias_variable(shape):
     """bias_variable generates a bias variable of a given shape."""
     initial = tf.constant(0.1, shape=shape)
     return tf.Variable(initial)
 
-# IMPLEMENT THIS
-# NOTE: FOR ALL THE FOLLOWING CODES, DO NOT IMPLEMENT YOUR OWN VERSION. 
-# USE THE BUILT-IN METHODS FROM TENSORFLOW.
-# Take a look at TensorFlow API Docs.
+# Wrapper for 2D convolution
 def conv2d(x, W):
   """conv2d returns a 2d convolution layer with full stride."""
   # Input x is 4d [image_num, x, y, chan] (chan = color) [?,28,28,1]
@@ -70,7 +68,7 @@ def conv2d(x, W):
   padding = 'SAME' # full zeropadding
   return tf.nn.conv2d(x,W,stride,padding)
 
-# IMPLEMENT THIS
+# Wrapper for maxpooling
 def max_pool_2x2(x):
   """max_pool_2x2 downsamples a feature map by 2X."""
   # Input x is 4d [image_num, x, y, chan] (chan = color) [?,28,28,1]
@@ -106,43 +104,14 @@ def getNMISTData():
     
     return x_train, y_train, x_test, y_test
 
-'''
-Build the CNN
 
-First Convolutional Layer[10 pts]
-We can now implement our first layer. It will consist of convolution, followed 
-by max pooling. The convolution will compute 32 features for each 5x5 patch. 
-Its weight tensor will have a shape of [5, 5, 1, 32]. The first two dimensions
- are the patch size, the next is the number of input channels, and the last is
- the number of output channels. We will also have a bias vector with a 
- component for each output channel.
-
-To apply the layer, we first reshape x to a 4d tensor, with the second and 
-third dimensions corresponding to image width and height, and the final 
-dimension corresponding to the number of color channels.
-
-We then convolve x_image with the weight tensor, add the bias, apply the ReLU 
-function, and finally max pool. The max_pool_2x2 method will reduce the image 
-size to 14x14.
-
-Second Convolutional Layer[5 pts]
-In order to build a deep network, we stack several layers of this type. The
- second layer will have 64 features for each 5x5 patch.
- 
-Fully Connected Layer[10 pts]
-Now that the image size has been reduced to 7x7, we add a fully-connected 
-layer with 1024 neurons to allow processing on the entire image. We reshape 
-the tensor from the pooling layer into a batch of vectors, multiply by a 
-weight matrix, add a bias, and apply a ReLU.
-
-SoftmaxLayer[5 pts]
-Finally, we add a layer of softmax regression
-'''
-
+# Build the CNN template.  This is a 3-hidden layer convolutional neural
+# network. It has 2 layers of convolution+pooling, followed by two feed-forward
+# (dense) layers.
 def deepnn(x):
     """
     deepnn builds the graph for a deep net for classifying digits.
-    Args:
+    Inputs:
       x: an input tensor with the dimensions (N_examples, 784), where 784 is the
       number of pixels in a standard MNIST image.
     Returns:
@@ -213,7 +182,7 @@ if __name__ == "__main__":
     if os.path.exists(checkpointSaveDir): # only rm if it exists
         shutil.rmtree(checkpointSaveDir, ignore_errors=True)    
     
-    # Get the NMIST data
+    # Get the NMIST handwritten-digit data
     x_train, y_train, x_test, y_test = getNMISTData()
 
     # Placeholders for the data and associated truth
@@ -223,16 +192,12 @@ if __name__ == "__main__":
     # Build the graph for the deep net
     # It is best to literally thing of this as just building the graph, since
     # it is in reality just a "placeholder" or template for what we will
-    # actually be running.
+    # actually be running. y_conv is [-1,10], where -1 is the number of input
+    # datapoints and 10 is the probability (logit) for each output class 
+    # (numeral).
     y_conv = deepnn(x)
     
-    '''
-    We can specify a loss function just as easily. Loss indicates how bad the 
-    model's prediction was on a single example; we try to minimize that while 
-    training across all the examples. Here, our loss function is the cross-entropy 
-    between the target and the softmax activation function applied to the model's 
-    prediction. As in the beginners tutorial, we use the stable formulation:
-    '''
+    #define the loss function (externally from the actual neural net)
     # This is literally just defining the loss function, not actually running it.
     with tf.name_scope('loss'):
         cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv)
@@ -240,65 +205,43 @@ if __name__ == "__main__":
     with tf.name_scope('adam_optimizer'):
         train_step = tf.train.AdamOptimizer().minimize(cross_entropy)
         
-    '''
-    First we'll figure out where we predicted the correct label. tf.argmax is an 
-    extremely useful function which gives you the index of the highest entry in a 
-    tensor along some axis. For example, tf.argmax(y,1) is the label our model 
-    thinks is most likely for each input, while tf.argmax(y_,1) is the true label. 
-    We can use tf.equal to check if our prediction matches the truth.
-    
-    That gives us a list of booleans. To determine what fraction are correct, we 
-    cast to floating point numbers and then take the mean. For example, [True, 
-    False, True, True] would become [1,0,1,1] which would become 0.75.
-    '''
+    # Accuracy calculation
     with tf.name_scope('accuracy'):
-        #correct_prediction = tf.argmax(y_,1)
-        #our_prediction = tf.argmax(y_conv,1)
-        #performance_vec = tf.equal(correct_prediction,our_prediction)
-        #accuracy = tf.reduce_mean(tf.cast(performance_vec, tf.float32))
         correct_prediction = tf.argmax(y_,1)
         y = tf.nn.softmax(y_conv)
         our_prediction = tf.argmax(y,1)
         performance_vec = tf.equal(correct_prediction,our_prediction)
         accuracy = tf.reduce_mean(tf.cast(performance_vec,tf.float32))
         
-    # For saving the graph, DO NOT CHANGE.
-    # This both saves the current state and gives us a way to run the model
-    # after training.
+    # Save the grap of the neural network and loss function
     print('Saving graph to: %s' % checkpointSaveDir)
     train_writer = tf.summary.FileWriter(checkpointSaveDir)
     train_writer.add_graph(tf.get_default_graph())
     
-    '''
-    Train and Evaluate the Model[5 pts]
+    # Initialize class to save the CNN post-training
+    saver = tf.train.Saver()
     
-    We will use a more sophisticated ADAM optimizer instead of a Gradient Descent 
-    Optimizer.
-    
-    We will add logging to every 100th iteration in the training process.
-    
-    Feel free to run this code. Be aware that it does 20,000 training iterations 
-    and may take a while (possibly up to half an hour), depending on your processor.
-    
-    The final test set accuracy after running this code should be approximately 
-    99.2%.
-    
-    We have learned how to quickly and easily build, train, and evaluate a fairly 
-    sophisticated deep learning model using TensorFlow.
-    
-    '''
-    
+    # Actually execute the training using the CNN template we just built
     with tf.Session() as sess:
+        # Initialize all variables
         sess.run(tf.global_variables_initializer())
+        
+        # Loop over every epoch
         for epoch in range(2000): 
-            batch = makeBatch(100, x_train, y_train, epoch)
+            batch = extractBatch(100, x_train, y_train, epoch)
             if epoch % 100 == 0:
                 train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1]})
                 print('epoch %d, training accuracy %g' % (epoch, train_accuracy))
-            train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+            train_step.run(feed_dict={x: batch[0], y_: batch[1]})    
     
-        # Finish off by running the test set
-        test_batch = makeBatch(len(x_test), x_test, y_test, 0)
+            # Save the model weights (and everything else) every 500 epochs
+            if epoch % 500 == 0:
+                save_path = saver.save(sess, checkpointSaveDir + "/model_at" + str(epoch) + ".ckpt")
+                print("Model saved in path: %s" % save_path)
+
+    
+        # Finish off by running the test set.  Extract the entire test set.
+        test_batch = extractBatch(len(x_test), x_test, y_test, 0)
         print('test accuracy %g' % accuracy.eval(feed_dict={x: test_batch[0], y_: test_batch[1]}))
         
         # Run a single testpoint through with use_cnn.py
