43ef64c3f01c8db45e2a481cefc789333ff063cc
James Kenneth Dunn <jkdunn@geo.bu.edu>
2019-09-28 16:03:19 -0400

    Partially done augment_sequence() function

--- videoUtilities.py
+++ videoUtilities.py
@@ -15,6 +15,7 @@ import cv2
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
 """
 FUNCTION:
@@ -58,7 +59,6 @@ def pull_video(inFile):
     finally:       
         # Save it off so we can play the video
         print("Exiting cleanly")
-        outStream.release()
     
     return outVideo
     
@@ -79,11 +79,13 @@ OPTIONAL INPUTS:
     iStart: first frame to read (default 0)
     iEnd: last frame to read (default -1, read until all gone)
     color: set to True to read in color (default False)
+    invert: set to True to invert black and white (default False)
     
 RETURNS: 
     Video from inFile as a [nframes,nx,ny,nColors] 4D array
 """
-def pull_sequence(inFileBase, ext='.jpg', iStart=0, iEnd=-1, color=False):
+def pull_sequence(inFileBase, ext='.jpg', iStart=0, iEnd=-1, color=False, 
+                  invert=False):
     index = iStart
     
     # Read first frame to get sizes
@@ -94,7 +96,8 @@ def pull_sequence(inFileBase, ext='.jpg', iStart=0, iEnd=-1, color=False):
         if not color:
             frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
             #frame = frame[:,:,0] # extract red channel
-            frame = cv2.bitwise_not(frame) # fix inverted colors
+            if invert:
+                frame = cv2.bitwise_not(frame) # fix inverted colors
             nColors = 1
         else:
             nColors = frame.shape[3]
@@ -123,7 +126,8 @@ def pull_sequence(inFileBase, ext='.jpg', iStart=0, iEnd=-1, color=False):
             if not color:
                 frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
                 #frame = frame[:,:,0] # extract red channel
-                frame = cv2.bitwise_not(frame) # fix inverted colors
+                if invert:
+                    frame = cv2.bitwise_not(frame) # fix inverted colors
             frame = np.reshape(frame,[1,width,height,nColors]) 
             #print("Frame dimensions: (%d,%d,%d,%d)" % frame.shape)
             outVideo = np.concatenate([outVideo,frame],axis=0) # tack on
@@ -138,15 +142,121 @@ def pull_sequence(inFileBase, ext='.jpg', iStart=0, iEnd=-1, color=False):
     return outVideo
     
     
-
+"""
+FUNCTION:
+    augment_sequence() 
+    
+DESCRIPTION:
+    Uses cv2 to pull a video and the corresponding truth mask frame-by-frame 
+    from a series of jpg files, then applies random scaling and cropping to
+    extract a region of a given shape matching the desired NN input shape.
+    This CAN and SHOULD be able to completely remove the target from the image.
+    We want to have examples with no target present in our training sets!
+    Finally a series of random noise, brightness, contrast, and rotation 
+    augmentations are performed. This produces a bevy of augmented images for 
+    each true input image.  Results are saved to file. Every output image has
+    the same dimensions, as set by the outShape parameter.
+    
+INPUTS: 
+    inImageFileBase: path+prefix to the desired image sequence. For example, to 
+        read all frames named "frame_####.jpg", set inImageFileBase="frame_"
+    inMaskFileBase: path+prefix to the desired mask sequence that goes with 
+        inImageFileBase. For example, to read all masks named "mask_####.jpg",
+        set inMaskFileBase="mask_".
+    outputFolder: path to the folder where the augmented images and masks will
+        be saved to.
+OPTIONAL INPUTS:
+    ext: filename extension, including '.' (default '.jpg')
+    iStart: first frame to read (default 0)
+    iEnd: last frame to read (default -1, read until all gone)
+    color: set to True to read in color (default False)
+    invert: set to True to invert black and white (default False)
+    outShape: forced [width,height] of the augmented images (default [256,256])
+    
+OUTPUTS:
+    Writes augmented versions of each input image and corresponding mask to
+    the outputFolder with the name "augmented_####_a****.jpg", where #### 
+    inherits from the frame number of the input parent image, and **** uniquely
+    identifies the augmentation applied.
+RETURNS: 
+    None
+"""
+def augment_sequence(inImageFileBase, inMaskFileBase, outputFolder,
+                     ext='.jpg', iStart=0, iEnd=-1, color=False, 
+                     invert=False, outShape=[256,256]):
+    
+    # Make output directory if it does not yet exist
+    if not os.path.exists(outputFolder):
+        os.mkdir(outputFolder)
+    
+    # Read in the images to augment one-by-one to preserve memory
+    rawIndex = iStart
+    while True:
+        if rawIndex >= iEnd and iEnd != -1:
+            break
+        
+        # Use pull_sequence with iStart=iEnd to just get one at a time
+        rawImage = pull_sequence(inImageFileBase, iStart=rawIndex, iEnd=rawIndex,
+                                 ext=ext, color=color, invert=invert)
+        rawMask = pull_sequence(inMaskFileBase, iStart=rawIndex, iEnd=rawIndex,
+                                 ext=ext, color=False, invert=False)
+        if len(rawImage.shape) == 1:
+            if rawImage == -1 or rawMask == -1:
+                break # we reached the end or had problems reading
+            
+        iAugmentation = 0
+        
+        # Apply some random crops and resizes to the raw images and masks, 
+        # always applying the same crop and resize to the image and its
+        # corresponding mask.
+        nbRandomCropResize = 8
+        
+        for iCropResize in range(nbRandomCropResize):
+            resizedImage = cv2.resize(np.squeeze(rawImage), (512,512))
+            croppedImage = resizedImage[:outShape[0],:outShape[1]]
+            finalAugmentedImage = croppedImage
+            resizedMask = cv2.resize(np.squeeze(rawMask), (512,512))
+            croppedMask = resizedMask[:outShape[0],:outShape[1]]
+            finalAugmentedMask = croppedMask
+            
+            # Write the augmented image and mask to file
+            augImageFileStr = os.path.join(outputFolder,
+                'augImage_%04d_%04d' % (rawIndex, iAugmentation) + ext)
+            augMaskFileStr = os.path.join(outputFolder,
+                'augMask_%04d_%04d' % (rawIndex, iAugmentation) + ext)
+            cv2.imwrite(augImageFileStr,np.squeeze(finalAugmentedImage))
+            cv2.imwrite(augMaskFileStr, np.squeeze(finalAugmentedMask))
+            iAugmentation += 1
+        
+        rawIndex += 1
+    # end while loop over raw images
+    
+# end augment_sequence()
     
 # Testing here
 if __name__ == "__main__":
+    '''
     myVideo = pull_sequence(os.path.join("sequences",
-        "defaultGreenscreenVideo_over_PHO_hallway","frame_"), iStart=330, iEnd=360)
+        "defaultGreenscreenVideo_over_PHO_hallway","frame_"), 
+        iStart=330, iEnd=360, invert = True)
     print("nFrames: %d" % myVideo.shape[0])
     
     plt.imshow(np.squeeze(myVideo[0,:,:,:]),cmap='Greys')
     plt.show()
+    '''
+    '''
+    myMaskVideo = pull_sequence(os.path.join("sequences",
+        "defaultGreenscreenVideo_over_PHO_hallway","mask_"), iStart=330, iEnd=360)
+    print("nFrames: %d" % myVideo.shape[0])
     
+    plt.imshow(np.squeeze(myMaskVideo[0,:,:,:]),cmap='Greys')
+    plt.show()
+    '''
+    
+    if not os.path.exists("augmentedSequences"):
+        os.mkdir("augmentedSequences")
+    augment_sequence(os.path.join("sequences","defaultGreenscreenVideo_over_PHO_hallway","frame_"),
+                     os.path.join("sequences","defaultGreenscreenVideo_over_PHO_hallway","mask_"),
+                     os.path.join("augmentedSequences","defaultGreenscreenVideo_over_PHO_hallway"),
+                     iStart=330, iEnd=332)
     
