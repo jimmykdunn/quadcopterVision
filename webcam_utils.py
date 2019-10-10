eabfdbbb505201a65a5fd7e543ad7411e1068559
Jimmy Dunn <jkdunn@bu.edu>
2019-10-10 17:07:10 -0400

    Basic working webcam capture in python

--- webcam_utils.py
+++ webcam_utils.py
@@ -17,40 +17,69 @@ import cv2
 import time
 from datetime import datetime
 import os
+ 
+"""
+grabStream()
+DESCRIPTION:
+    Simple webcam test - grabs numFrames from webcam and saves them to the 
+    designated folder as jpg images.
+    
+OPTIONAL INPUTS:
+    folder: directory to write the captured frames to
+    numFrames: total number of frames to grab
+    
+OUTPUTS: 
+    Saves the captured frames to file
 
-# Using https://github.com/andfoy/textobjdetection/blob/master/ssd/demo/live.py as an example
-def grabStream(folder='.',width=320,height=240,numFrames=100):
+INFO:
+    Author: James Dunn, Boston University
+    Thesis work for MS degree in ECE
+    Advisor: Dr. Roberto Tron
+    Email: jkdunn@bu.edu
+    Date: October 2019
+    
+REFERENCES:
+   Used https://github.com/andfoy/textobjdetection/blob/master/ssd/demo/live.py
+   as an example.
+"""
+def grabStream(folder='.',numFrames=100):
     # Initialize
     print("Initializing video stream")
     stream = WebcamVideoStream(src=0)
-    #stream.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-    #stream.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
     
     # Start the video stream
     stream.start()
     time.sleep(1.0) # wait for stream to initialize
-    video = np.zeros((numFrames,width,height))
     print("Video stream initialized")
     
+    # Create the output folder if it does not yet exist
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+    print("Saving frames to " + folder) 
+    
+    # Loop over the number of desired frames at maximum speed
     print("Capturing %d frames" % numFrames)
     start_time = datetime.now()
-    #while True:
     for i in range(numFrames):
-        video[i,:,:] = stream.read() # grab next frame
+        frame = stream.read() # grab next frame
+        
+        # Save the frame to file
+        filestr = "frame_%04d.jpg" % i
+        fullpath = os.path.join(folder, filestr)
+        cv2.imwrite(fullpath, frame)
+        print("Wrote " + fullpath + ' ' + str(frame.shape))
+    # end capture
+        
+    # Calculate framerate
     end_time = datetime.now()  
     timeElapsed = (end_time-start_time).total_seconds()
     
     print("%d frames captured in %g seconds: framerate = %g Hz" % 
         (numFrames,timeElapsed,numFrames/timeElapsed))
-        
-    # Save out at the end   
-    print("Saving frames to " + folder) 
-    for i in range(numFrames):
-        filestr = "frame_%04d" % i
-        fullpath = os.path.join(folder, filestr+'.jpg')
-        cv2.imwrite(np.squeeze(video[i,:,:]),fullpath)
 
     print("Complete!")
+# end grabStream
 
+# Run grabStream if called directly
 if __name__ == "__main__":
-    grabStream()
+    grabStream(folder='webcam')
