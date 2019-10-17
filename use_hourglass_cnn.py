f9eeb433a3f8fa01f04839e9616fad487b3c668c
James Kenneth Dunn <jkdunn@scc2.bu.edu>
2019-10-17 12:01:43 -0400

    Changing up golden images to be 64x48

--- use_hourglass_cnn.py
+++ use_hourglass_cnn.py
@@ -366,5 +366,5 @@ if __name__ == "__main__":
     #print("\n\n")
     #quadcopterTest(os.path.join('homebrew_hourglass_nn_save','model_at750.ckpt'))
     print("\n\n")
-    #quadcopterBatchTest(os.path.join('homebrew_hourglass_nn_save','model_at10.ckpt'))
-    quadcopterBatchTest(os.path.join('homebrew_hourglass_nn_save','model_at10.pb'))
+    quadcopterBatchTest(os.path.join('homebrew_hourglass_nn_save','model_at625.ckpt'))
+    #quadcopterBatchTest(os.path.join('homebrew_hourglass_nn_save','model_at10.pb'))
