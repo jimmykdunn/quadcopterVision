b4fc3cdcdc3b4d3bacb12e29cca3bbeaa068a6c1
James Kenneth Dunn <jkdunn@scc-pi7.scc.bu.edu>
2019-10-29 16:27:01 -0400

    First cut at temporal siamese network

--- run_siamese_hourglass_cnn.py
+++ run_siamese_hourglass_cnn.py
@@ -18,7 +18,6 @@ import tensorflow as tf
 import numpy as np
 import os
 import shutil
-import mnist
 import time
 import cv2
 import copy
@@ -37,25 +36,33 @@ as the sum of the difference between the mask and the heatmap and the difference
 between the heatmaps of the two siamese networks. Ends with a test forward pass.
 "-1" is a placeholder for the number of images in the batch.
 INPUTS:
-    trainImages: input images, [-1,nx,ny,1] 
-    trainMasks: truth masks associated with trainImages. +1 if target, -1 otherwise, [-1,nx,ny] 
-    testImages: test images, [-1,nx,ny,1]
-    testMasks: truth masks associated with testImages. +1 if target, -1 otherwise, [-1,nx,ny] 
+    trainImagesA: input images, [-1,nx,ny,1] 
+    trainMasksA: truth masks associated with trainImages. +1 if target, -1 otherwise, [-1,nx,ny] 
+    testImagesA: test images, [-1,nx,ny,1]
+    testMasksA: truth masks associated with testImages. +1 if target, -1 otherwise, [-1,nx,ny] 
+    trainImagesB: input images for 2nd arm of Siamese network, [-1,nx,ny,1]
+    trainMasksB: truth masks for 2nd arm of Siamese network associated with 
+                 trainImages. +1 if target, -1 otherwise, [-1,nx,ny] 
+    testImagesB: test images for 2nd arm of Siamese network, [-1,nx,ny,1]
+    testMasksB: truth masks for 2nd arm of Siamese network associated with 
+                 testImages. +1 if target, -1 otherwise, [-1,nx,ny]
 OPTIONAL INPUTS:
     nEpochs: number of training epcohs to run (default 100)
     batchSize: number of images to use per training epoch (default 100)
     checkpointSaveDir: directory to save trained neural net and graph to (default './hourglass_nn_save")
     saveEveryNEpochs: save checkpoint every this many epochs (and at the end) (default 500)
     peekEveryNEpochs: print the training gain with a forward pass every this many epochs (default 50)
+    siameseWeight: weight to apply to siamese deltas relative to heatmap gain (default 1.0)
 EXAMPLE:
     test_heatmaps = train_hourglass_nn(x)
 RETURNS:
     test_heatmaps: map of pixel values for each image, higher for pixels more 
                    likely to be target. Same size as input testImages.
 """
-def train_siamese_hourglass_nn(trainImages, trainMasks, testImages, testMasks, \
+def train_siamese_hourglass_nn(trainImagesA, trainMasksA, testImagesA, testMasksA, \
+                               trainImagesB, trainMasksB, testImagesB, testMasksB, \
     nEpochs=100, batchSize=100, checkpointSaveDir='./hourglass_nn_save', \
-    saveEveryNEpochs=500, peekEveryNEpochs=50):
+    saveEveryNEpochs=500, peekEveryNEpochs=50, siameseWeight = 1.0):
     
     print("BEGIN SIAMESE HOURGLASS NN TRAINING")
     
@@ -66,7 +73,13 @@ def train_siamese_hourglass_nn(trainImages, trainMasks, testImages, testMasks, \
         os.mkdir(checkpointSaveDir)
         
     # Image sizes
-    nBatch, nWidth, nHeight = trainImages.shape[:3]
+    nBatch,  nWidth,  nHeight  = trainImagesA.shape[:3]
+    nBatchB, nWidthB, nHeightB = trainImagesB.shape[:3]
+    
+    # Check that the two arms of the Siamese network have identical shapes
+    assert nBatch  == nBatchB
+    assert nWidth  == nWidthB
+    assert nHeight == nHeightB
 
     # Placeholders for the data and associated truth
     # "b_" prefix stands for "batch"
@@ -77,12 +90,22 @@ def train_siamese_hourglass_nn(trainImages, trainMasks, testImages, testMasks, \
     # Build the graph for the deep hourglass net
     # It is best to literally thing of this as just building the graph, since
     # it is in reality just a "placeholder" or template for what we will
-    # actually be running. y_conv is [-1,10], where -1 is the number of input
-    # datapoints and 10 is the probability (logit) for each output class 
-    # (numeral).
+    # actually be running.
     with tf.name_scope('heatmaps'):
-        b_heatmaps = nns.hourglass_nn(b_images)
-        b_heatmaps = tf.reshape(b_heatmaps,[-1,nWidth,nHeight],'b_heatmaps')
+        # b_images here is the concatenation of both arms of the siamese
+        # network, but we will only use the heatmaps from the first half
+        # (the first arm) as the output
+        b_heatmapsAll = nns.hourglass_nn(b_images)
+        b_heatmapsAll = tf.reshape(b_heatmapsAll,[-1,nWidth,nHeight],'b_heatmapsAll')
+        
+    with tf.name_scope('siameseSplit'):
+        # Split off into 
+        heatmapShape = tf.shape(b_heatmapsAll)
+        nBatch = tf.cast(tf.divide(heatmapShape[0],2),tf.int32)
+        b_heatmapsA = b_heatmapsAll[:nBatch,:,:] # arm A heatmaps
+        b_heatmapsB = b_heatmapsAll[nBatch:,:,:] # arm B heatmaps
+        b_masksA = b_masks[:nBatch,:,:] # arm A masks
+        #b_masksB = b_masks[nBatch:,:,:] # arm B masks
    
     # The heatmap loss calculation
     with tf.name_scope('heatmapGain'):
@@ -92,29 +115,40 @@ def train_siamese_hourglass_nn(trainImages, trainMasks, testImages, testMasks, \
         # To do this, targetmask must have +1's at target     locations
         # and         targetmask must have -1's at background locations
         # Make sure targetmask is formed in this way!!!
-        b_gainmaps = tf.multiply(b_heatmaps, b_masks) # pixel-by-pixel gain
+        b_gainmaps = tf.multiply(b_heatmapsA, b_masksA) # pixel-by-pixel gain
         b_gainmaps = tf.math.minimum(b_gainmaps, 1.0, name="b_gainmaps") # anything above 1 doesn't help
         
         # May be useful to have an intermediate reduction here of a single
         # gain value for each individual image...
         
         # Average of gain across every pixel of every image
-        gain = tf.reduce_mean(tf.cast(b_gainmaps,tf.float32))
-        loss = tf.multiply(-1.0,gain)
+        heatmapGain = tf.reduce_mean(tf.cast(b_gainmaps,tf.float32))
+        heatmapLoss = tf.multiply(-1.0,heatmapGain)
         
         # Perfect segementation would result in this gain value
         booleanMask = tf.math.greater(b_masks,0)
         perfectGain = tf.reduce_mean(tf.cast(booleanMask,tf.float32))
+    
+    with tf.name_scope('siameseGain'):    
+        # Calculate difference maps between each arm of the siamese network.
+        # Square to make positive and for mathematical niceness.
+        b_siameseDelta = tf.subtract(b_heatmapsA,b_heatmapsB)
+        b_siameseDelta = tf.square(b_siameseDelta)
+        siameseLoss = tf.reduce_mean(tf.cast(b_siameseDelta,tf.float32))
+        
+    with tf.name_scope('overallLoss'):
+        # Overall loss is just the sum of the heatmap loss and weighted
+        # siamese loss
+        loss = tf.add(heatmapLoss, tf.multiply(siameseWeight, siameseLoss))
+        
         
     # Optimization calculation
     with tf.name_scope('adam_optimizer'):
         # Basic ADAM optimizer
         train_step = tf.train.AdamOptimizer().minimize(loss)
         
-    # Final accuracy calculation is nothing more than the gain itself. No need
-    # for an additional calculation.
         
-    # Save the grap of the neural network and loss function
+    # Save the graph of the neural network and loss function
     print('Saving graph to: %s' % checkpointSaveDir)
     train_writer = tf.summary.FileWriter(checkpointSaveDir)
     train_writer.add_graph(tf.get_default_graph())
@@ -137,20 +171,24 @@ def train_siamese_hourglass_nn(trainImages, trainMasks, testImages, testMasks, \
         testGainHistory = []
         for epoch in range(nEpochs): 
             # Extract data for this batch
-            batch = nnu.extractBatch(batchSize, trainImages, trainMasks, epoch)
+            batchA = nnu.extractBatch(batchSize, trainImagesA, trainMasksA, epoch)
+            batchB = nnu.extractBatch(batchSize, trainImagesB, trainMasksB, epoch)
             
             # Run a single epoch with the extracted data batch
-            train_step.run(feed_dict={b_images: batch[0], b_masks: batch[1]}) 
+            batchImages = tf.concat([batchA[0], batchB[0]], 0) # concatenate siamese arms
+            batchMasks = tf.concat([batchA[1], batchB[1]], 0) # concatenate siamese arms
+            train_step.run(feed_dict={b_images: batchImages, b_masks: batchMasks}) 
             
             # Check our progress on the training data every peekEveryNEpochs epochs
             if epoch % peekEveryNEpochs == (peekEveryNEpochs-1):
-                trainGain = gain.eval(feed_dict={b_images: batch[0], b_masks: batch[1]})
-                perfectTrainGain = perfectGain.eval(feed_dict={b_images: batch[0], b_masks: batch[1]})
-                print('epoch %d of %d, training gain %g' % (epoch+1, nEpochs, trainGain/perfectTrainGain))
-                testBatch = nnu.extractBatch(100, testImages, testMasks, 0, randomDraw=True)
-                testGain = gain.eval(feed_dict={b_images: testBatch[0], b_masks: testBatch[1]})
+                trainGain = heatmapGain.eval(feed_dict={b_images: batchImages, b_masks: batchMasks})
+                perfectTrainGain = perfectGain.eval(feed_dict={b_images: batchImages, b_masks: batchMasks})
+                trainLoss = loss.eval(feed_dict={b_images: batchImages, b_masks: batchMasks})
+                print('epoch %d of %d, training heatmap gain %g, training loss' % (epoch+1, nEpochs, trainGain/perfectTrainGain, trainLoss))
+                testBatch = nnu.extractBatch(100, testImagesA, testMasksA, 0, randomDraw=True)
+                testGain = heatmapGain.eval(feed_dict={b_images: testBatch[0], b_masks: testBatch[1]})
                 perfectTestGain = perfectGain.eval(feed_dict={b_images: testBatch[0], b_masks: testBatch[1]})
-                print('epoch %d of %d, test gain %g' % (epoch+1, nEpochs, testGain/perfectTestGain))
+                print('epoch %d of %d, test heatmap gain %g' % (epoch+1, nEpochs, testGain/perfectTestGain))
                 peekSchedule.append(epoch+1)
                 trainGainHistory.append(trainGain/perfectTrainGain)
                 testGainHistory.append(testGain/perfectTestGain)
@@ -181,10 +219,10 @@ def train_siamese_hourglass_nn(trainImages, trainMasks, testImages, testMasks, \
               % (nEpochs, end_sec-start_sec))
     
         # Finish off by running the test set.  Extract the entire test set.
-        test_batch = nnu.extractBatch(len(testImages), testImages, testMasks, 0)
-        test_gain = gain.eval(feed_dict={b_images: test_batch[0], b_masks: test_batch[1]})
+        test_batch = nnu.extractBatch(len(testImagesA), testImagesA, testMasksA, 0)
+        test_gain = heatmapGain.eval(feed_dict={b_images: test_batch[0], b_masks: test_batch[1]})
         perfectTestGain = perfectGain.eval(feed_dict={b_images: test_batch[0], b_masks: test_batch[1]})
-        test_heatmaps = b_heatmaps.eval(feed_dict={b_images: test_batch[0], b_masks: test_batch[1]})
+        test_heatmaps = b_heatmapsAll.eval(feed_dict={b_images: test_batch[0], b_masks: test_batch[1]})
         print('test gain %g' % (test_gain/perfectTestGain))
         
         # Print the location of the saved network
@@ -249,7 +287,6 @@ if __name__ == "__main__":
     y_all = np.concatenate([y_set1,y_set2,y_set3,y_set4,y_set5,y_set6,y_set7,y_set8],axis=0)
     id_all = np.concatenate([id_set1,id_set2,id_set3,id_set4,id_set5,id_set6,id_set7,id_set8],axis=0)
     '''
-    '''
     x_all, y_all, id_all = vu.pull_aug_sequence(
         os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab1_64x48","augImage_"),
         os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab1_64x48","augMask_"))
@@ -257,36 +294,48 @@ if __name__ == "__main__":
     x_all, y_all, id_all = vu.pull_aug_sequence(
         os.path.join("augmentedContinuousSequences","sortTest","augImage_"),
         os.path.join("augmentedContinuousSequences","sortTest","augMask_"))
+    '''
     
     x_all_s, y_all_s, id_all_s = vu.sort_aug_sequence(x_all, y_all, id_all)
     
-    # !!!!LEFT OFF HERE 191023!!!
-    # Generate a logical train/test split on the images - probably makes the
-    # most sense to pull the final 4 augmentations over all time so that we
-    # get a full set.  Could pull the final N frames over all augmentations too.
-    # 
-    # After that is complete, split the images into [0:-5,:,:] and [5:,:,:]
-    # sets for direct feeding into the siamese network.  We can then easily
-    # run two simultaneous nns.hourglass_nn() on those two sets.
-    # The tricky part will be ensuring that the same weights are used on both
-    # sides, which probably means creating them early and supplying to 
-    # a copy of hourglass_nn() as inputs.
+    # Split into train and test sets temporally: first bunch to train, second
+    # bunch to test
+    nAugs, nFrames, width, height = x_all_s.shape
+    trainFraction = 0.75
+    nTrain = int(nFrames * trainFraction)
+    x_train = x_all_s[:,:nTrain,:,:] # [nAugs,nframes,nx,ny]
+    y_train = y_all_s[:,:nTrain,:,:]
+    x_test  = x_all_s[:,nTrain:,:,:]
+    y_test  = y_all_s[:,nTrain:,:,:]
     
-    
-    
-    # Split into train and test sets randomly
-    #x_train, y_train, x_test, y_test = \
-    #    vu.train_test_split(x_all, y_all, trainFraction=0.8)
-    x_train, y_train, x_test, y_test = \
-        vu.train_test_split_noCheat(x_all, y_all, id_all, trainFraction=0.8)
-
     # Convert masks to appropriately-weighted +/- masks
     y_train_pmMask = nnu.booleanMaskToPlusMinus(y_train, falseVal=-0.01)
     y_test_pmMask  = nnu.booleanMaskToPlusMinus(y_test, falseVal=-0.01)
     
+    # Assemble each siamese arm. This is the 1-frame delta temporal siamese version.
+    x_trainA        = x_train       [:,:-1,:,:] # extract all but last temporal frame
+    y_train_pmMaskA = y_train_pmMask[:,:-1,:,:] # extract all but last temporal frame
+    x_testA         = x_test        [:,:-1,:,:] # extract all but last temporal frame
+    y_test_pmMaskA  = y_test_pmMask [:,:-1,:,:] # extract all but last temporal frame
+    x_trainB        = x_train       [:,1: ,:,:] # extract all but first temporal frame
+    y_train_pmMaskB = y_train_pmMask[:,1: ,:,:] # extract all but first temporal frame
+    x_testB         = x_test        [:,1: ,:,:] # extract all but first temporal frame
+    y_test_pmMaskB  = y_test_pmMask [:,1: ,:,:] # extract all but first temporal frame
+    
+    # Flatten the first two dimensions of everything (augmentation and temporal frame)
+    x_trainA        = x_trainA.reshape       (-1, *x_trainA.shape[-2:])
+    y_train_pmMaskA = y_train_pmMaskA.reshape(-1, *y_train_pmMaskA.shape[-2:])
+    x_testA         = x_testA.reshape        (-1, *x_testA.shape[-2:])
+    y_test_pmMaskA  = y_test_pmMaskA.reshape (-1, *y_test_pmMaskA.shape[-2:])
+    x_trainB        = x_trainB.reshape       (-1, *x_trainB.shape[-2:])
+    y_train_pmMaskB = y_train_pmMaskB.reshape(-1, *y_train_pmMaskB.shape[-2:])
+    x_testB         = x_testB.reshape        (-1, *x_testB.shape[-2:])
+    y_test_pmMaskB  = y_test_pmMaskB.reshape (-1, *y_test_pmMaskB.shape[-2:])
     
     # Run the complete training on the hourglass neural net
-    heatmaps = train_siamese_hourglass_nn(x_train, y_train_pmMask, x_test, y_test_pmMask, 
+    heatmaps = train_siamese_hourglass_nn(
+            x_trainA, y_train_pmMaskA, x_testA, y_test_pmMaskA, 
+            x_trainB, y_train_pmMaskB, x_testB, y_test_pmMaskB, 
         checkpointSaveDir = checkpointSaveDir, peekEveryNEpochs = peekEveryNEpochs,
         saveEveryNEpochs=saveEveryNEpochs, nEpochs=nEpochs, batchSize=batchSize)
         
