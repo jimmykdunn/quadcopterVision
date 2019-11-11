b1a86bac9d0cda5c3caccdde2b3bf2c19373350c
James Kenneth Dunn <jkdunn@scc2.bu.edu>
2019-11-11 11:06:47 -0500

    Adding confusion matrix analysis script. Set up n-fold CV next.

---
+++ analyzePerformance.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+"""
+FILE: analyzePerformance.py
+DESCRIPTION:
+    Functions for performance analysis on quadcopter drone heatmaps
+
+INFO:
+    Author: James Dunn, Boston University
+    Thesis work for MS degree in ECE
+    Advisor: Dr. Roberto Tron
+    Email: jkdunn@bu.edu
+    Date: November 2019
+"""
+
+from cv2.dnn import readNetFromTensorflow
+import numpy as np
+import os
+from importData import importRoboticsLabData
+
+
+"""
+calculateConfusionMatrix()
+    Calculates the confusion matrix for the input heatmaps and associated truth
+    masks via applying the input threshold to classify heatmap pixels as target
+    or non-target.
+INPUTS:
+    heatmaps: array of heatmaps [nBatch,width,height]
+    masks: array of truth masks [nBatch,width,height]
+    threshold: threshold to apply. Heatmap pixels > threshold deemed target
+RETURNS:
+    2x2 confusion matrix [nbTruePos, nbFalsePos; nbFalseNeg, nbTrueNeg]
+"""
+# Confusion matrix calculation
+def calculateConfusionMatrix(heatmaps, masks, threshold):
+
+    # Apply the threshold to the heatmaps to generate a binary true (target) vs
+    # false (non-target) for each pixel.
+    heatmapClass = heatmaps > threshold
+    maskClass = masks > 0
+    
+    # Compare classification of each pixel to the truth mask
+    truePosPixels = np.logical_and(heatmapClass,maskClass);
+    falsePosPixels = np.logical_and(heatmapClass,np.logical_not(maskClass));
+    falseNegPixels = np.logical_and(np.logical_not(heatmapClass),maskClass);
+    trueNegPixels = np.logical_and(np.logical_not(heatmapClass),np.logical_not(maskClass));
+    
+    # Calculate totals
+    tp = np.sum(truePosPixels)
+    fn = np.sum(falseNegPixels)
+    fp = np.sum(falsePosPixels)
+    tn = np.sum(trueNegPixels)
+    
+    return [tp,fn,fp,tn]
+# end calculateConfusionMatrix
+
+
+# Convert numerical confusion matrix to fractional
+def confusionMatrixNumToPct(confusionMatrix):
+
+    tpRate = confusionMatrix[0]/(confusionMatrix[0]+confusionMatrix[1]) # target called target/total targets
+    fpRate = confusionMatrix[1]/(confusionMatrix[0]+confusionMatrix[1]) # noise called target/total targets
+    fnRate = confusionMatrix[2]/(confusionMatrix[2]+confusionMatrix[3]) # target called noise/total noise
+    tnRate = confusionMatrix[3]/(confusionMatrix[2]+confusionMatrix[3]) # noise called noise/total noise
+    
+    return [tpRate, fpRate, fnRate, tnRate]
+
+# end confusionMatrixNumToPct
+    
+def runBasicPerformanceAnalysis(modelPath):
+    # Import the augmented robotics lab data sequences
+    print("Reading augmented image and mask sequences")
+    x_all, y_all, id_all, id_all_plus = importRoboticsLabData(quickTest=True)
+    
+    # Import the trained neural network
+    print("Loading saved neural network from " + modelPath+'.pb')
+    tensorflowNet = readNetFromTensorflow(modelPath+'.pb')
+    print("Neural network sucessfully loaded")
+    
+    # Test on a set of frames
+    #tensorflowNet.setInput(x_all[:100,:,:])
+    #heatmapsTest = tensorflowNet.forward() 
+    
+    # Execute a forward pass on all of the input frames
+    heatmaps = np.zeros_like(x_all) # preallocate for speed
+    for i, frame in enumerate(x_all):
+        if i % 100 == 0:
+            print("Running CNN on frame %d of %d" % (i, x_all.shape[0]))
+        tensorflowNet.setInput(frame)
+        heatmap = tensorflowNet.forward()
+        heatmap = np.squeeze(heatmap)*255.0 # scale appropriately
+        
+        heatmaps[i,:,:] = heatmap # put into the big stack
+    
+    
+    # Calculate the resulting confusion matrix
+    confMatrix = calculateConfusionMatrix(heatmaps, y_all, 0.5)
+    
+    # Print class statistics
+    print('')
+    print("Total number of target pixels: %11d" % confMatrix[0]+confMatrix[1])
+    print("Total number of noise  pixels: %11d" % confMatrix[2]+confMatrix[3])
+    print("#Noise Pixels/#TargetPixels = %g" % (confMatrix[2]+confMatrix[3])/(confMatrix[0]+confMatrix[1]))
+    
+    # Print raw confusion matrix
+    print('')
+    print("Confusion matrix (pixel counts)")
+    print("             call target,  call noise")
+    print("true target  %11d, %11d" % (confMatrix[0], confMatrix[1]))
+    print("true noise   %11d, %11d" % (confMatrix[2], confMatrix[3]))
+    
+    # Convert the confusion matrix to percentages and print
+    print('')
+    confMatrixPct = confusionMatrixNumToPct(confMatrix)
+    print("Confusion matrix (percentages)")
+    print("             call target,  call noise")
+    print("true target        %5.2g,       %5.2g" % (confMatrixPct[0], confMatrixPct[1]))
+    print("true noise         %5.2g,       %5.2g" % (confMatrixPct[2], confMatrixPct[3]))
+    
+# end runBasicPerformanceAnalysis
+    
+    
+# Run if called directly
+if __name__ == "__main__":
+    runBasicPerformanceAnalysis(os.path.join('homebrew_hourglass_nn_save_GOOD','modelFinal_full_mirror_sW00p50_1M00p00_2M00p00'))
--- importData.py
+++ importData.py
@@ -20,6 +20,9 @@ importRoboticsLabData()
     Imports all robotics lab data
 INPUTS:
     None
+OPTIONAL INPUTS:
+    quickTest: Set to true to just import one of the videos for quick debugging
+    tests. Default False.
 RETURNS:
     x_all: all input images with injected quadrotors (including augmentations)
     y_all: all associated boolean truth masks
@@ -30,65 +33,52 @@ RETURNS:
             "****_%%%%_$$" where **** and %%%% are as above, and $$ represents
             the index of the video that each frame came from.
 """
-def importRoboticsLabData():
-    # Complete datasets
+def importRoboticsLabData(quickTest=False):
+    
     x_set1, y_set1, id_set1 = vu.pull_aug_sequence(
         os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab1_64x48_mirror","augImage_"),
         os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab1_64x48_mirror","augMask_"))
     id_set1_plus = [id+"_01" for id in id_set1]
-    x_set2, y_set2, id_set2 = vu.pull_aug_sequence(
-        os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab2_64x48_mirror","augImage_"),
-        os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab2_64x48_mirror","augMask_"))
-    id_set2_plus = [id+"_02" for id in id_set2]
+    x_all = x_set1
+    y_all = y_set1
+    id_all = id_set1
+    id_all_plus = id_set1_plus
+        
+    if not quickTest:
+        x_set2, y_set2, id_set2 = vu.pull_aug_sequence(
+            os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab2_64x48_mirror","augImage_"),
+            os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab2_64x48_mirror","augMask_"))
+        id_set2_plus = [id+"_02" for id in id_set2]
+        x_set3, y_set3, id_set3 = vu.pull_aug_sequence(
+            os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab3_64x48_mirror","augImage_"),
+            os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab3_64x48_mirror","augMask_"))
+        id_set3_plus = [id+"_03" for id in id_set3]
+        x_set4, y_set4, id_set4 = vu.pull_aug_sequence(
+            os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab4_64x48_mirror","augImage_"),
+            os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab4_64x48_mirror","augMask_"))
+        id_set4_plus = [id+"_04" for id in id_set4]
+        x_set5, y_set5, id_set5 = vu.pull_aug_sequence(
+            os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab1_64x48_baby_mirror","augImage_"),
+            os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab1_64x48_baby_mirror","augMask_"))
+        id_set5_plus = [id+"_05" for id in id_set5]
+        x_set6, y_set6, id_set6 = vu.pull_aug_sequence(
+            os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab2_64x48_baby_mirror","augImage_"),
+            os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab2_64x48_baby_mirror","augMask_"))
+        id_set6_plus = [id+"_06" for id in id_set6]
+        x_set7, y_set7, id_set7 = vu.pull_aug_sequence(
+            os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab3_64x48_baby_mirror","augImage_"),
+            os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab3_64x48_baby_mirror","augMask_"))
+        id_set7_plus = [id+"_07" for id in id_set7]
+        x_set8, y_set8, id_set8 = vu.pull_aug_sequence(
+            os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab4_64x48_baby_mirror","augImage_"),
+            os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab4_64x48_baby_mirror","augMask_"))
+        id_set8_plus = [id+"_08" for id in id_set8]
     
-    x_set3, y_set3, id_set3 = vu.pull_aug_sequence(
-        os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab3_64x48_mirror","augImage_"),
-        os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab3_64x48_mirror","augMask_"))
-    id_set3_plus = [id+"_03" for id in id_set3]
-    x_set4, y_set4, id_set4 = vu.pull_aug_sequence(
-        os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab4_64x48_mirror","augImage_"),
-        os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab4_64x48_mirror","augMask_"))
-    id_set4_plus = [id+"_04" for id in id_set4]
-    x_set5, y_set5, id_set5 = vu.pull_aug_sequence(
-        os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab1_64x48_baby_mirror","augImage_"),
-        os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab1_64x48_baby_mirror","augMask_"))
-    id_set5_plus = [id+"_05" for id in id_set5]
-    x_set6, y_set6, id_set6 = vu.pull_aug_sequence(
-        os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab2_64x48_baby_mirror","augImage_"),
-        os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab2_64x48_baby_mirror","augMask_"))
-    id_set6_plus = [id+"_06" for id in id_set6]
-    x_set7, y_set7, id_set7 = vu.pull_aug_sequence(
-        os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab3_64x48_baby_mirror","augImage_"),
-        os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab3_64x48_baby_mirror","augMask_"))
-    id_set7_plus = [id+"_07" for id in id_set7]
-    x_set8, y_set8, id_set8 = vu.pull_aug_sequence(
-        os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab4_64x48_baby_mirror","augImage_"),
-        os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab4_64x48_baby_mirror","augMask_"))
-    id_set8_plus = [id+"_08" for id in id_set8]
-    
-    x_all = np.concatenate([x_set1,x_set2,x_set3,x_set4,x_set5,x_set6,x_set7,x_set8],axis=0)
-    y_all = np.concatenate([y_set1,y_set2,y_set3,y_set4,y_set5,y_set6,y_set7,y_set8],axis=0)
-    id_all = np.concatenate([id_set1,id_set2,id_set3,id_set4,id_set5,id_set6,id_set7,id_set8],axis=0)
-    id_all_plus = np.concatenate([id_set1_plus,id_set2_plus,id_set3_plus,id_set4_plus,id_set5_plus,id_set6_plus,id_set7_plus,id_set8_plus],axis=0)
-    
-    '''
-    x_all = np.concatenate([x_set1,x_set2])
-    y_all = np.concatenate([y_set1,y_set2])
-    id_all = np.concatenate([id_set1,id_set2])
-    id_all_plus = np.concatenate([id_set1_plus,id_set2_plus])
-    '''
-    '''
-    # Smaller datasets for faster debugging    
-    x_all, y_all, id_all = vu.pull_aug_sequence(
-        os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab1_64x48","augImage_"),
-        os.path.join("augmentedContinuousSequences","defaultGreenscreenVideo_over_roboticsLab1_64x48","augMask_"))
-    id_all_plus = [id+"_01" for id in id_all]
-    '''
-    '''
-    x_all, y_all, id_all = vu.pull_aug_sequence(
-        os.path.join("augmentedContinuousSequences","sortTest","augImage_"),
-        os.path.join("augmentedContinuousSequences","sortTest","augMask_"))
-    '''   
+        x_all = np.concatenate([x_set1,x_set2,x_set3,x_set4,x_set5,x_set6,x_set7,x_set8],axis=0)
+        y_all = np.concatenate([y_set1,y_set2,y_set3,y_set4,y_set5,y_set6,y_set7,y_set8],axis=0)
+        id_all = np.concatenate([id_set1,id_set2,id_set3,id_set4,id_set5,id_set6,id_set7,id_set8],axis=0)
+        id_all_plus = np.concatenate([id_set1_plus,id_set2_plus,id_set3_plus,id_set4_plus,id_set5_plus,id_set6_plus,id_set7_plus,id_set8_plus],axis=0)
+    # end if not quickTest
     
     return x_all, y_all, id_all, id_all_plus
     
